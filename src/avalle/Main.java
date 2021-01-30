7aeb0cd7de5fbef90cc72517565b215ca4beb85e
jmounteer <mounteerjaden@gmail.com>
2021-01-29 17:50:32 -0700

    Add change.

--- src/avalle/Main.java
+++ src/avalle/Main.java
@@ -5,8 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello my name is Carlos");
-
-
         System.out.println("Hi Carlos! My name is Jaden.");
     }
 }
