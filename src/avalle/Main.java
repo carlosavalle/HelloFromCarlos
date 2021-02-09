68f77ef7014570ad91b8cdf52de861c0135b7f9d
John Leo Natividad <johnleofraxidio@gmail.com>
2021-02-10 00:13:25 +1100

    Update by John

--- src/avalle/Main.java
+++ src/avalle/Main.java
@@ -6,5 +6,6 @@ public class Main {
 	// write your code here
         System.out.println("Hello my name is Carlos");
         System.out.println("Hi Carlos! My name is Jaden.");
+        System.out.println("Hi Carlos! My name is John.");
     }
 }
